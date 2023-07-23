773af259fbafe9b0c0b9dde312630c6c617bd81d
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-07-22 22:40:42 -0300

    quedo joya, arregle el carrusel

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const grande    = document.querySelector('.contenedor')
+const punto     = document.querySelectorAll('.punto')
+
+
 $(document).ready(function () {
     
     $('body').on({'mousemove'   : function (e) {
@@ -28,3 +32,28 @@ $(document).ready(function () {
 
 
 })
+
+// Recorrer TODOS los punto
+punto.forEach( ( cadaPunto , i )=> {
+    // Asignamos un CLICK a cadaPunto
+    punto[i].addEventListener('click',()=>{
+
+        // Guardar la posición de ese PUNTO
+        let posicion  = i
+        // Calculando el espacio que debe DESPLAZARSE el GRANDE
+        let operacion = posicion * -25
+
+        // MOVEMOS el grand
+        grande.style.transform = `translateX(${ operacion }%)`
+
+        // Recorremos TODOS los punto
+        punto.forEach( ( cadaPunto , i )=>{
+            // Quitamos la clase ACTIVO a TODOS los punto
+            punto[i].classList.remove('activo')
+        })
+        // Añadir la clase activo en el punto que hemos hecho CLICK
+        punto[i].classList.add('activo')
+
+    })
+})
+
