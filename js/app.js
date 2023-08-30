e2e487345d22bbe5d253b4beae0d555b1b0c7aeb
olimarteam <73653954+olimarteam@users.noreply.github.com>
2023-08-30 02:04:14 -0300

    ahora quedo bien, las img no son cliqueables pero de todas formas, los proyectos estan en github

--- js/app.js
+++ js/app.js
@@ -33,11 +33,3 @@ $(document).ready(function () {
 
 })
 
-var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 5){
-        counter = 1;
-    }
-}, 5000);
